9579b22f66c8875d6b64adc9c5e4251e82e7d698
Fly0kart <94280213+Fly0kart@users.noreply.github.com>
2021-11-15 19:12:12 +0530

    Add files via upload

---
+++ src/components/AboutUs/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import MeetTheTeam from './MeetTheTeam/MeetTheTeam'
+import Overview from './Overview/Overview';
+
+const About = () => {
+  return (
+    <div>
+      <Overview />
+      <MeetTheTeam />
+    </div>
+  );
+}
+
+export default About;
